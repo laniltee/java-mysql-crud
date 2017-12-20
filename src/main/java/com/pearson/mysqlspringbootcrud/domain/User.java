52dd9ec422b0cf17a6fab6296f6b2c502c263b60
Lanil <lanil.marasinghe@pearson.com>
2017-12-20 10:25:51 +0530

    first commit

---
+++ src/main/java/com/pearson/mysqlspringbootcrud/domain/User.java
@@ -0,0 +1,9 @@
+package com.pearson.mysqlspringbootcrud.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class User {
+}
---
+++ src/main/java/com/pearson/mysqlspringbootcrud/web/UserController.java
@@ -0,0 +1,9 @@
+package com.pearson.mysqlspringbootcrud.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class UserController {
+}
